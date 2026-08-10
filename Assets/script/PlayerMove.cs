b117b6f571ad325cec741caa38737832bff60522
Bruni Tang <bruniaxu@gmail.com>
2026-08-10 18:46:43 -0400

    made player moves works

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -15,22 +15,11 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if we have a Vector2 and we want to add the a Vector3
-        //We can convert between them using (Vector3) in front of the Vector2
         transform.position += (Vector3)movementDirection * speed * Time.deltaTime;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movementDirection = context.ReadValue<Vector2>();
-    }
-
-    public void OnAttack(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Debug.Log("attack! " + context.phase);
-        }
-       
     }
 }
