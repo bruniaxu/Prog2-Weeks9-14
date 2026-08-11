e391dbed812b3a0a2b7e578865dff282e3559652
Bruni Tang <bruniaxu@gmail.com>
2026-08-10 20:12:19 -0400

    finished four shrines

--- Assets/script/ShrineEffectTest.cs
+++ Assets/script/ShrineEffectTest.cs
@@ -4,12 +4,37 @@ public class ShrineEffectTest : MonoBehaviour
 {
     public SpriteRenderer playerRenderer;
 
-    // Called by the shrine UnityEvent
+    //Sprites for the four elemental forms
+    public Sprite fireSprite;
+    public Sprite waterSprite;
+    public Sprite natureSprite;
+    public Sprite lightSprite;
+
+    //Called by the Fire Shrine UnityEvent
     public void FireEffect()
     {
-        // Change player colour to red
-        playerRenderer.color = Color.red;
+        playerRenderer.sprite = fireSprite;
+        Debug.Log("Fire form activated!");
+    }
 
-        Debug.Log("Fire effect activated!");
+    //Called by the Water Shrine UnityEvent
+    public void WaterEffect()
+    {
+        playerRenderer.sprite = waterSprite;
+        Debug.Log("Water form activated!");
+    }
+
+    //Called by the Nature Shrine UnityEvent
+    public void NatureEffect()
+    {
+        playerRenderer.sprite = natureSprite;
+        Debug.Log("Nature form activated!");
+    }
+
+    //Called by the Light Shrine UnityEvent
+    public void LightEffect()
+    {
+        playerRenderer.sprite = lightSprite;
+        Debug.Log("Light form activated!");
     }
 }
